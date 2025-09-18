081543f4d54b77a3313aeacfc61d89fbd3e5d673
Johann A. K.  Lindele <johann.lindele@kohl-digital.de>
2025-09-18 13:09:20 +0200

    118

--- src/app/books/book.actions.ts
+++ src/app/books/book.actions.ts
@@ -3,5 +3,8 @@ import { Book } from "../models/book";
 
 // action to add a book
 export const AddBook = createAction('[Book] Add Book', props<Book>());
+// success and failure actions for async operations (if needed) *EFFECTS*
+export const AddBookSuccess = createAction('[Book] Add Book Success', props<Book>());
+export const AddBookFailure = createAction('[Book] Add Book Failure', props<{ error: any }>());
 // action to remove a book by id
 export const RemoveBook = createAction('[Book] Remove Book', props<{ bookId: string }>());
